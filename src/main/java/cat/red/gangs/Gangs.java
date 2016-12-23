7944365b4313e7aa43cf94c545d5abd69ace4122
Nathan DELHAYE <ndelhaye@shopbot-inc.com>
2016-12-23 13:07:40 +0100

    First

---
+++ src/main/java/cat/red/gangs/Gangs.java
@@ -0,0 +1,15 @@
+package cat.red.gangs;
+
+import org.slf4j.Logger;
+import org.spongepowered.api.world.storage.WorldProperties;
+
+@Plugin(id = PluginInfo.ID, name = PluginInfo.NAME, version = PluginInfo.VERSION, description = PluginInfo.DESCRIPTION )
+public class Gangs
+{
+	protected Gangs()
+	{
+		;
+	}
+
+	private static Gangs gangs;
+}
---
+++ src/main/java/cat/red/gangs/PluginInfo.java
@@ -0,0 +1,9 @@
+package cat.red.gangs;
+
+public abstract class PluginInfo
+{
+	public static final String ID = "cat.red.gangs";
+	public static final String NAME = "Gangs";
+	public static final String VERSION = "0.0.1";
+	public static final String DESCRIPTION = "This plugin add gangs which are teams of one or more user and can claim land";
+}
